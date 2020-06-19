0763c0a52b2879f32b60017d11d90402478e3005
Jessica Isoton <jessicasampaio195@gmail.com>
2020-06-19 16:32:35 +0200

    5.3: bloglist frontend, step3

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -38,6 +41,20 @@ const App = () => {
       window.alert("Wrong credentials");
     }
   };
+
+  const handleCreateBlog = async (event) => {
+    event.preventDefault();
+    try {
+      await blogService.create({
+        title,
+        author,
+        url
+      });
+    } catch (exception) {
+      window.alert("The system couldn't create a new blog ");
+    }
+    window.alert("Created");
+  }
 
   const loginForm = () => (
     <form onSubmit={handleLogin}>
@@ -71,6 +88,39 @@ const App = () => {
     </div>
   );
 
+  const showCreateNewBlog = () => (
+    <form onSubmit={handleCreateBlog}>
+      <div>
+        title
+        <input
+          type="text"
+          value={title}
+          name="title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author
+        <input
+          type="text"
+          value={author}
+          name="author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url
+        <input
+          type="text"
+          value={url}
+          name="url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  );
+
   const handleLogout = () => {
     window.localStorage.removeItem("loggedBlogappUser");
     window.location.reload();
@@ -90,6 +140,7 @@ const App = () => {
               logout
             </button>
           </p>
+          {showCreateNewBlog()}
           {showBlogs()}
         </div>
       )}
